12027bac8b8ca671bdabd82594ec0f56446d8972
estherbarbara <tekmedeiros@gmail.com>
2017-03-24 16:35:16 -0300

    Cliente companhia

--- src/Cliente.java
+++ src/Cliente.java
@@ -8,23 +8,35 @@ import java.util.Scanner;
 public class Cliente {
 
 	public static void main (String args[]) throws MalformedURLException, AlreadyBoundException, NotBoundException {
-		Geral obj;
-		Geral obj1;
+		Geral obj1, obj2, obj3, obj4;
 		Scanner reader = new Scanner(System.in);
 		try {
 			// recuperando o objeto remoto via o servidor de nomes
-			obj = (Geral) Naming.lookup("rmi://localhost/HelloWordServer");
-			obj1 = (Geral) Naming.lookup("rmi://localhost/Teste");
+			obj1 = (Geral) Naming.lookup("rmi://localhost/A");
+			obj2 = (Geral) Naming.lookup("rmi://localhost/B");
+			obj3 = (Geral) Naming.lookup("rmi://localhost/C");
+			obj4 = (Geral) Naming.lookup("rmi://localhost/D");
 			// invocando o método hello do servidor remoto
+			while(true){
 				System.out.println("Bem vindo, qual companhia aéra deseja selecionar?");
+				System.out.println("1 - A"
+						+ "2 - B"
+						+ "3 - C"
+						+ "4 - D");
 				int n = reader.nextInt();
-				if(n == 1){
-					System.out.println(obj1.hello("asd"));
-					obj1.incial();
+				switch(n){
+					case 1:
+						System.out.println(obj1.opcoesAereas());
+					case 2:
+						System.out.println(obj2.opcoesAereas());
+					case 3:
+						System.out.println(obj3.opcoesAereas());
+					case 4:
+						System.out.println(obj4.opcoesAereas());
 				}
-				if(n == 2)
-					System.out.println(obj.hello("teste"));	
 			}
+			
+		}
 		catch (RemoteException e) {
 			System.out.println("Erro" + e);
 		}
