0a06b861f61ce574c8fd379e68a4b1fdb7ccccd4
Thiagolink <t.t.2007@hotmail.com>
2017-03-24 18:56:16 -0300

    Finalizado

--- src/Cliente.java
+++ src/Cliente.java
@@ -7,38 +7,105 @@ import java.util.Scanner;
 
 public class Cliente {
 
-	public static void main (String args[]) throws MalformedURLException, AlreadyBoundException, NotBoundException {
+	Scanner reader = new Scanner(System.in);
+	
+	public static void main (String args[]) throws MalformedURLException, AlreadyBoundException, NotBoundException, RemoteException {
 		Geral obj1, obj2, obj3, obj4;
-		Scanner reader = new Scanner(System.in);
-		try {
-			// recuperando o objeto remoto via o servidor de nomes
-			obj1 = (Geral) Naming.lookup("rmi://localhost/A");
-			obj2 = (Geral) Naming.lookup("rmi://localhost/B");
-			obj3 = (Geral) Naming.lookup("rmi://localhost/C");
-			obj4 = (Geral) Naming.lookup("rmi://localhost/D");
+		Geral objEscolhido = (Geral) Naming.lookup("rmi://localhost/B");
+
+		// recuperando o objeto remoto via o servidor de nomes
+		obj1 = (Geral) Naming.lookup("rmi://localhost/A");
+		obj2 = (Geral) Naming.lookup("rmi://localhost/B");
+		obj3 = (Geral) Naming.lookup("rmi://localhost/C");
+		obj4 = (Geral) Naming.lookup("rmi://localhost/D");
+		
+		int n;
+		boolean reiniciaReserva = false;
+		do{
+			Scanner reader = new Scanner(System.in);
 			// invocando o método hello do servidor remoto
-			while(true){
+			boolean repeteIteracao;
+			do{
+				repeteIteracao = false;
 				System.out.println("Bem vindo, qual companhia aéra deseja selecionar?");
-				System.out.println("1 - A"
-						+ "2 - B"
-						+ "3 - C"
-						+ "4 - D");
-				int n = reader.nextInt();
+				System.out.println("1 - A\n"
+						+ "2 - B\n"
+						+ "3 - C\n"
+						+ "4 - D\n");
+				n = reader.nextInt();
 				switch(n){
 					case 1:
+						objEscolhido = obj1;
 						System.out.println(obj1.opcoesAereas());
+						break;
 					case 2:
+						objEscolhido = obj2;
 						System.out.println(obj2.opcoesAereas());
+						break;
 					case 3:
+						objEscolhido = obj3;
 						System.out.println(obj3.opcoesAereas());
+						break;
 					case 4:
+						objEscolhido = obj4;
 						System.out.println(obj4.opcoesAereas());
+						break;
+					default:
+						repeteIteracao = true;
+						System.out.println("Por favor, entre apenas com números de 1 até 7");
 				}
-			}
+			}while(repeteIteracao);
 			
-		}
-		catch (RemoteException e) {
-			System.out.println("Erro" + e);
-		}
+			do{
+				repeteIteracao = false;
+				System.out.println("1 - Vôos com escala" + "\n" + "2 - Vôos sem escala");
+				n = reader.nextInt();
+				if(n!=1 && n!=2){
+					repeteIteracao = true;
+					System.out.println("Por favor, entre apenas com 1 ou 2");
+				}
+			}while(repeteIteracao);
+
+			System.out.println(objEscolhido.mostraPassagens( n==1 ? true : false));
+			System.out.println("Faça sua reserva entrando com o dígito respectivo a passagem que deseja");
+
+			int m;
+			do{
+				repeteIteracao = false;
+				m = reader.nextInt();
+				if( ((m<1 || m>5) && n==2 ) || ((m<6 || m>7) && n==1)){
+					repeteIteracao = true;
+					if(n==2)
+						System.out.println("Por favor, entre apenas com números de 1 até 5");
+					if(n==1)
+						System.out.println("Por favor, entre apenas com números de 6 até 7");
+				}
+			}while(repeteIteracao);
+			
+			System.out.println( objEscolhido.fazReserva(m, n==1 ? true : false) );
+			
+			System.out.println("Deseja realizar outra reserva em outra companhia aérea? \n"
+					+ "1 - Sim \n"
+					+ "2 - Não");
+
+			do{
+				repeteIteracao = false;
+				n = reader.nextInt();
+				if( n!=1 && n!=2){
+					repeteIteracao = true;
+					System.out.println("Por favor, entre apenas com 1 ou 2");
+				}
+			}while(repeteIteracao);
+			
+			if(n==1)
+				reiniciaReserva = true;
+			else
+				reiniciaReserva = false;
+			
+			//reader.close();
+			
+		}while(reiniciaReserva);
+
 	}
+	
 }
